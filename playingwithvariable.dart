c54c71403c9fd287e99badb70d1c9fc0dd75012a
Avimanyu Rimal <127291200+avimanyurimal@users.noreply.github.com>
2023-12-18 23:26:22 +0545

    Dart_First_Commit

---
+++ playingwithvariable.dart
@@ -0,0 +1,11 @@
+void main(){
+  int age = 20;
+  String name = "Avimanyu Rimal";
+  double height = 5.7;
+
+  int addingYear = 10;
+  int futureAge = age + addingYear;
+
+  print("My name is $name and iam now $age year old");
+  print("My age after 10 year will be $futureAge ");
+}
